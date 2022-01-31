e45ca9ed025ea43f1340eb06b91c5e44429e4ed1
Timothy Lin <timothy.lin@cylynx.io>
2022-01-31 15:41:20 +0800

    convert cite.js to esm

--- src/cite.js
+++ src/cite.js
@@ -1,17 +1,61 @@
 // Copied as is from https://github.com/larsgw/citation.js/blob/main/index.js
 // commented out doi, ris, wikidata as they are not used in the rehype plugin
-const core = require('@citation-js/core')
 
-require('@citation-js/plugin-bibjson')
-require('@citation-js/plugin-bibtex')
-require('@citation-js/plugin-csl')
+import * as core from '@citation-js/core/lib/index.js'
+import { parsers as coreParsers } from '@citation-js/core/lib/plugin-common/input/index.js'
+import coreOutput from '@citation-js/core/lib/plugin-common/output/index.js'
+
+import '@citation-js/plugin-bibjson/lib/index.js'
+import '@citation-js/plugin-bibtex/lib/index.js'
+import '@citation-js/plugin-csl/lib/index.js'
+import { fetchEngine } from '@citation-js/plugin-csl/lib/engines.js'
+import { parsers as bibjsonParsers } from '@citation-js/plugin-bibjson/lib/index.js'
+import { formats } from '@citation-js/plugin-bibtex/lib/input/index.js'
+import * as entries from '@citation-js/plugin-bibtex/lib/input/entries.js'
+import * as bibtxt from '@citation-js/plugin-bibtex/lib/input/bibtxt.js'
+import bibtexOutput from '@citation-js/plugin-bibtex/lib/output/index.js'
+import * as bibtexOutputEntries from '@citation-js/plugin-bibtex/lib/output/entries.js'
+import { Converters } from '@citation-js/plugin-bibtex/lib/mapping/shared.js'
+import { format as bibtxtFormat } from '@citation-js/plugin-bibtex/lib/output/bibtxt.js'
+
 // require('@citation-js/plugin-doi')
 // require('@citation-js/plugin-ris')
 // require('@citation-js/plugin-wikidata')
 
-const citeproc = require('citeproc')
-const name = require('@citation-js/name')
-const date = require('@citation-js/date')
+import citeproc from 'citeproc'
+import * as name from '@citation-js/name'
+import * as date from '@citation-js/date'
+
+// import bibtexTypes from '@citation-js/plugin-bibtex/lib/mapping/bibtexTypes.json'
+const bibtexTypes = {
+  source: {
+    article: 'article-journal',
+    book: 'book',
+    booklet: 'book',
+    conference: 'paper-conference',
+    inbook: 'chapter',
+    incollection: 'chapter',
+    inproceedings: 'paper-conference',
+    mastersthesis: 'thesis',
+    phdthesis: 'thesis',
+    proceedings: 'book',
+    techreport: 'report',
+    unpublished: 'manuscript',
+  },
+  target: {
+    article: 'article',
+    'article-journal': 'article',
+    'article-magazine': 'article',
+    'article-newspaper': 'article',
+    book: 'book',
+    chapter: 'inbook',
+    manuscript: 'unpublished',
+    'paper-conference': 'inproceedings',
+    report: 'techreport',
+    review: 'article',
+    'review-book': 'article',
+  },
+}
 
 function clone(obj) {
   const copy = {}
@@ -80,7 +124,7 @@ Cite.version = {
 const CSL = core.plugins.config.get('@csl')
 
 Cite.CSL = {
-  engine: require('@citation-js/plugin-csl/lib/engines').fetchEngine,
+  engine: fetchEngine,
   item(data) {
     return (id) => data.find((entry) => entry.id === id)
   },
@@ -123,7 +167,7 @@ Cite.parse = Object.assign(
     date: date.parse,
     csl: core.plugins.input.util.clean,
 
-    bibjson: require('@citation-js/plugin-bibjson').parsers.json.record,
+    bibjson: bibjsonParsers.json.record,
     bibtex: ((parsers, entries, types) => ({
       json(entries) {
         return entries.parse([].concat(entries))
@@ -144,15 +188,11 @@ Cite.parse = Object.assign(
       type(type) {
         return types[type] || 'book'
       },
-    }))(
-      require('@citation-js/plugin-bibtex/lib/input').formats,
-      require('@citation-js/plugin-bibtex/lib/input/entries'),
-      require('@citation-js/plugin-bibtex/lib/mapping/bibtexTypes.json').target
-    ),
+    }))(formats, entries, bibtexTypes.target),
     bibtxt: ((bibtxt) => ({
       text: bibtxt.parse,
       textEntry: bibtxt.textEntry,
-    }))(require('@citation-js/plugin-bibtex/lib/input/bibtxt')),
+    }))(bibtxt),
     // doi: ((doi) => ({
     //   api: doi.parsers.api.parse,
     //   id: doi.parsers.id.parse,
@@ -160,7 +200,7 @@ Cite.parse = Object.assign(
     //     api: doi.parsers.api.parseAsync,
     //   },
     // }))(require('@citation-js/plugin-doi')),
-    json: require('@citation-js/core/lib/plugin-common/input').parsers.json.parse,
+    json: coreParsers.json.parse,
     // wikidata: ((wikidata) => ({
     //   json: wikidata.parsers.entity.parse,
     //   list: wikidata.parsers.id.parse,
@@ -197,17 +237,13 @@ Cite.get = Object.assign(
       type(type) {
         return types[type] || 'misc'
       },
-    }))(
-      require('@citation-js/plugin-bibtex/lib/output').default,
-      require('@citation-js/plugin-bibtex/lib/output/entries'),
-      require('@citation-js/plugin-bibtex/lib/mapping/shared').Converters,
-      require('@citation-js/plugin-bibtex/lib/mapping/bibtexTypes.json').target
-    ),
-    bibtxt: require('@citation-js/plugin-bibtex/lib/output/bibtxt').format,
-    json: require('@citation-js/core/lib/plugin-common/output').default.data,
-    label: require('@citation-js/core/lib/plugin-common/output').default.label,
+    }))(bibtexOutput, bibtexOutputEntries, Converters, bibtexTypes.target),
+    bibtxt: bibtxtFormat,
+    json: coreOutput.data,
+    label: coreOutput.label,
   },
   Cite.plugins.output
 )
 
-module.exports = Cite
+export default Cite
+// module.exports = Cite
