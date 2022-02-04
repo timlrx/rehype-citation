d7b08eebbe52ee9adea936f5542524f530dcf95c
Timothy Lin <timothy.lin@cylynx.io>
2022-02-04 22:12:46 +0800

    export Cite and generator function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import rehypeCitation from './src/index.js'
+import Cite from './src/cite.js'
+import rehypeCitationGenerator from './src/generator.js'
 
+export { Cite, rehypeCitationGenerator }
 export default rehypeCitation
