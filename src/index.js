ca5f9efea1d5003486de9a448675c3bcae08412a
Timothy Lin <timothy.lin@cylynx.io>
2022-02-05 16:12:52 +0800

    move csl from citation-js to standalone

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import rehypeCitationGenerator from './generator.js'
 // @ts-ignore
 import Cite from './cite.js'
-import mla from '../csl/mla.js'
-import chicago from '../csl/chicago.js'
+import mla from '../styles/mla.js'
+import chicago from '../styles/chicago.js'
+import harvard1 from '../styles/harvard1.js'
+import vancouver from '../styles/vancouver.js'
 
-// Citation.js comes with apa, harvard1 and vancouver
 const config = Cite.plugins.config.get('@csl')
+
+// Citation.js comes with apa
+config.templates.add('vancouver', vancouver)
+config.templates.add('harvard1', harvard1)
 config.templates.add('mla', mla)
 config.templates.add('chicago', chicago)
 
