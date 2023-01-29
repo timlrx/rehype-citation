371c96b806e4e145c9ab65d6895c08095149479c
Timothy Lin <timothy0336@hotmail.com>
2023-01-29 18:45:42 +0800

    fix: whitelist sr-only style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './demo/**/*.{js,ts,jsx,tsx}'],
+  safelist: ['sr-only'],
   theme: {
     extend: {},
   },
