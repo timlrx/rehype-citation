cadf8e0c7ea07251673d17ac0843bf9616b85015
Timothy Lin <timothy0336@hotmail.com>
2023-01-29 17:56:58 +0800

    refactor: add demo site

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 
-// Citation.js uses process and buffer which has to be polyfilled
 export default defineConfig({
   plugins: [react()],
   build: {
