f66d9d3b343b9d64e1e09957222c95af4adc0c6a
Timothy Lin <timothy0336@hotmail.com>
2023-02-05 15:15:14 +0800

    refactor: move md examples to separate file

--- demo/app/App.jsx
+++ demo/app/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import './index.css'
 import Example from './Example'
+import {
+  defaultExample,
+  SuppressBibliographyExample,
+  CustomCSLExample,
+  FootnotesExample,
+} from './md-examples'
 
 const bibliography =
   'https://raw.githubusercontent.com/timlrx/rehype-citation/main/test/references-data.bib'
@@ -10,61 +16,6 @@ const acmCSL =
 
 const chicagofullnoteCSL =
   'https://raw.githubusercontent.com/citation-style-language/styles/master/chicago-fullnote-bibliography.csl'
-
-const defaultExample = `## Welcome
-
-Rehype plugin to nicely format citations in markdown
-documents and insert bibliography in html format.
-
-Supports standard citations [@Nash1950], in-text citations [@Nash1951]
-and multiple citations [see @Nash1950 pp 12-13; @Nash1951]
-
-Customizable CSL and locale.
-
-### Bibliography
-`
-
-const SuppressBibliographyExample = `## Welcome
-
-Rehype plugin to nicely format citations in markdown
-documents and insert bibliography in html format.
-
-Supports standard citations [@Nash1950], in-text citations [@Nash1951]
-and multiple citations [see @Nash1950 pp 12-13; @Nash1951]
-
-Customizable CSL and locale.
-`
-
-const CustomCSLExample = `## Welcome
-
-Choose from one of the multiple preconfigured CSLs[^1]
-[^1]: apa, vancouver, harvard1, chicago, mla
-
-or pass in a valid file path or url to the _csl_ argument.
-
-Here's an example in ACM format:
-
-Supports standard citations [@Nash1950], in-text citations [@Nash1951]
-and multiple citations [@Nash1950; @Nash1951]
-
-### References
-`
-
-const FootnotesExample = `## Welcome
-
-The plugin also works with Github formatted footnotes
-from remarkgfm and automatically merges user content with citations.[^1]
-
-[^1]: There's no guarantee it works with other footnote formats since
-it searches for specific ids and hrefs added by GFM
-
-Here's an example in Chicago fullnote style:
-
-Supports standard citations [@Nash1950], in-text citations [@Nash1951]
-and multiple citations [@Nash1950; @Nash1951]
-
-### References
-`
 
 const examples = ['Default', 'Suppress Bibliography', 'Custom CSL', 'Footnote Style']
 
