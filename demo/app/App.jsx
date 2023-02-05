98281081cccc23bf5da21263032bd9945f84d2ce
Timothy Lin <timothy0336@hotmail.com>
2023-02-05 16:33:55 +0800

    add navigation

--- demo/app/App.jsx
+++ demo/app/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './index.css'
 import Example from './Example'
 import {
@@ -25,9 +25,16 @@ const examples = [
   'Footnote Style',
   'Link Citations',
 ]
+const pathList = examples.map((e) => e.toLocaleLowerCase().replace(' ', '-'))
 
 function App() {
-  const [selected, setSelected] = useState(examples[0])
+  useEffect(() => {
+    const result = pathList.find((x) => x === window.location.pathname.slice(1))
+    if (result) setSelected(examples[pathList.indexOf(result)])
+    else setSelected(examples[0])
+  }, [])
+
+  const [selected, setSelected] = useState()
   return (
     <>
       <body className="mx-6 pt-12 lg:pl-12 mb-12">
@@ -35,7 +42,10 @@ function App() {
         <div className="mt-8 space-x-4 space-y-2">
           {examples.map((x) => (
             <button
-              onClick={() => setSelected(x)}
+              onClick={() => {
+                window.history.pushState({}, '', x.toLowerCase().replace(' ', '-'))
+                setSelected(x)
+              }}
               className={`${
                 selected === x && 'bg-gradient-to-r from-purple-400 to-yellow-400'
               } px-4 py-2 text-sm font-bold bg-indigo-100 rounded`}
