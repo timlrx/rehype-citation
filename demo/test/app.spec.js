71118843bada46e8d33ab9bfc3c91ef51b158934
Timothy Lin <timothy0336@hotmail.com>
2023-02-12 12:34:38 +0800

    test: update e2e test

--- demo/test/app.spec.js
+++ demo/test/app.spec.js
@@ -4,5 +4,5 @@ import { test, expect } from '@playwright/test'
 test('should work in a client env', async ({ page }) => {
   await page.goto('http://localhost:3000')
   const locator = page.locator('id=results')
-  await expect(locator).toContainText('(Nash, 1950)')
+  await expect(locator).toContainText('(Nash, 1950, pp. iv, vi–xi, (xv)-(xvii))')
 })
